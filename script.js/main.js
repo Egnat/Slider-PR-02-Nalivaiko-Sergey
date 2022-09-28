6cb120badd4bca169e142f32064353e958dd2b32
Egnat <sergey_p_i@mail.ru>
2022-09-28 21:04:49 +0300

    initinal commit

---
+++ script.js/main.js
@@ -0,0 +1,32 @@
+/*let slideIndex = 1;
+        
+function positionSlides(n) {
+  showSlides(slideIndex += n);
+} 
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");  
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+for (let i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";  
+}
+for (let i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" active", "");
+}
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+showSlides(slideIndex);
+
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
+
+btnPrev.addEventListener("click", () => {positionSlides(-1);});
+btnNext.addEventListener("click", () => {positionSlides(1);});*/
